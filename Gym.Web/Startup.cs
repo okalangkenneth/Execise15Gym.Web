681f105abf647af749b5a78f591deb1fa0bbc87a
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-04-08 11:16:29 +0200

    Change default Route, Update Links

--- Gym.Web/Startup.cs
+++ Gym.Web/Startup.cs
@@ -76,7 +76,7 @@ namespace Gym.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=GymClasses}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
