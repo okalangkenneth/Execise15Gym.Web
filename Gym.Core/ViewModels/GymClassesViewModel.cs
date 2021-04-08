ef01af199242ef4ef1a94c688433b789f4401388
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-04-08 13:05:00 +0200

    Add ViewModels

---
+++ Gym.Core/ViewModels/GymClassesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Core.ViewModels
+{
+    public class GymClassesViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public bool Attending { get; set; }
+    }
+}
---
+++ Gym.Core/ViewModels/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Core.ViewModels
+{
+   public class IndexViewModel
+    {
+        public IEnumerable<GymClassesViewModel> GymClasses { get; set; }
+        public bool ShowHistory { get; set; }
+    }
+}
